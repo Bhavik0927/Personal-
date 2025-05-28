569e6add96de24d9a3baaa0c90bbd856a0979139
Bhavik <bhaviktembhare2@gmail.com>
2025-05-28 14:18:17 +0530

    delete blog api created

--- Backend/Routes/Blogs.js
+++ Backend/Routes/Blogs.js
@@ -30,7 +30,7 @@ const requireAuth = (req, res, next) => {
     next();
 };
 
-blogRoute.get('/view', requireAuth ,async (req, res) => {
+blogRoute.get('/view', requireAuth, async (req, res) => {
     try {
         const blogs = await Blog.find().populate('createdBy');
 
@@ -45,19 +45,36 @@ blogRoute.get('/view', requireAuth ,async (req, res) => {
     }
 })
 
-blogRoute.get('/myblog', async(req,res) =>{
+blogRoute.get('/myblog', async (req, res) => {
     try {
         const userId = req.user._id;
-        if(!userId){
+        if (!userId) {
             res.status(402).send("user is not login..login first")
         }
-        const response =await Blog.find({createdBy: userId});
-        if(!response || response.length === 0){
+        const response = await Blog.find({ createdBy: userId });
+        if (!response || response.length === 0) {
             res.status(404).send("Blogs are not created yet...");
         }
         res.status(200).json(response);
     } catch (error) {
         console.log(error.message);
+    }
+});
+
+
+blogRoute.delete('/:id', async (req, res) => {
+    try {
+        const blog = await Blog.findById(req.params.id);
+        console.log(blog);
+        if (!blog) return res.status(404).json({ message: 'Blog not found' });
+        if (blog.createdBy.toString() !== req.user._id) {
+            return res.status(404).json({ message: 'User is not authorized' })
+        }
+        await Blog.findByIdAndDelete(req.params.id);
+        res.status(200).json({message:'blog is successfully deleted'});
+
+    } catch (error) {
+        console.log(error);
     }
 })
 export default blogRoute;
